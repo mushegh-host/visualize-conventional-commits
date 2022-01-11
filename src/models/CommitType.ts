e348298bf5be366038948253e9b99eadd410a8f0
Mushegh Antaranyan <mushegh.antaranyan@smunch.co>
2022-01-12 00:27:31 +0400

    fix(model): parse commit type

--- src/models/CommitType.ts
+++ src/models/CommitType.ts
@@ -7,4 +7,34 @@ export enum CommitType {
   Refactor = "refactor",
   Documentation = "docs",
   Integration = "ci",
+  Revert = "revert",
 }
+
+export const parseCommitType = (input: unknown): CommitType => {
+  if (typeof input !== "string") {
+    throw new Error(`Expected string, got ${typeof input}`);
+  }
+
+  switch (input) {
+    case "fix":
+      return CommitType.Fix;
+    case "feat":
+      return CommitType.Feature;
+    case "chore":
+      return CommitType.Chore;
+    case "style":
+      return CommitType.Style;
+    case "test":
+      return CommitType.Test;
+    case "refactor":
+      return CommitType.Refactor;
+    case "docs":
+      return CommitType.Documentation;
+    case "ci":
+      return CommitType.Integration;
+    case "revert":
+      return CommitType.Revert;
+    default:
+      throw new Error(`Unknown commit type: ${input}`);
+  }
+};
