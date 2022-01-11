8ec9369189aaf366902875391accc9a20ca3baef
Mushegh Antaranyan <mushegh.antaranyan@smunch.co>
2022-01-12 00:13:15 +0400

    fix(model): introduce commit types

---
+++ src/models/CommitType.ts
@@ -0,0 +1,10 @@
+export enum CommitType {
+  Fix = "fix",
+  Feature = "feat",
+  Chore = "chore",
+  Style = "style",
+  Test = "test",
+  Refactor = "refactor",
+  Documentation = "docs",
+  Integration = "ci",
+}
